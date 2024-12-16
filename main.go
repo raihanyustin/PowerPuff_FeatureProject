33b2b7a95b81471fa651b6ad2e1888c3a1b54e25
safiyyah40 <safiyyahyahya163@gmail.com>
2024-12-16 21:39:16 +0700

    revisi akhir

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"PowerPuff_ReviewBarang/controllers/reviewcontroller"
+
+	"PowerPuff_ReviewBarang/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default();
+	models.ConnectDatabase()
+	
+	r.POST("/api/review/stack/push", reviewcontroller.PushToStack)
+	r.GET("/api/review/stack/peek", reviewcontroller.PeekStack)
+	r.GET("/api/review/stack/all", reviewcontroller.GetAllFromStack)
+	r.GET("/api/review/:ProductName", reviewcontroller.Show)
+	r.GET("/api/review/search", reviewcontroller.SearchByProductAndRating)
+	r.PUT("/api/review/:ID", reviewcontroller.Update)
+
+	r.Run()
+}
